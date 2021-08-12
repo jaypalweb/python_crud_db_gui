60dcc3ff2a380909f650f89b6bddd4d66a5b5587
Jay Pal <contactjaypal@gmail.com>
2021-08-12 23:25:46 +0530

    added some comments

--- mybooks.py
+++ mybooks.py
@@ -2,12 +2,14 @@ from tkinter import Tk, Button, Label, Scrollbar, Listbox, StringVar, Entry, W, 
 from tkinter import ttk
 from tkinter import messagebox
 
-root = Tk()
+root = Tk() # Creates application window
 
-root.title("My Books Database Application")
-root.configure(background="light green")
-root.geometry("850x500")
-root.resizable(width=False, height=False)
+root.title("My Books Database Application") # Add a title to application window
+root.configure(background="light green")    # Add background color to application window
+root.geometry("850x500")    # Sets a size for application window
+root.resizable(width=False, height=False)   # Prevents the applicatiion whinow from resizing
+
+# Create lables and entry widgets
 
 title_label = ttk.Label(root, text="Title", background="light green", font=("TkDefaultFont", 16))
 title_label.grid(row=0, column=0, sticky=W)
@@ -27,18 +29,23 @@ isbn_text =  StringVar()
 isbn_entry =  ttk.Entry(root, width=24, textvariable=isbn_text)
 isbn_entry.grid(row=0, column=5, sticky=W)
 
+# Add a button to insert inputs into database
+
 add_btn = Button(root, text="Add Book", bg="blue", fg="white", font="helvetica 10 bold", command="")
 add_btn.grid(row=0, column=6, sticky=W)
 
+# Add a listbox to didplay data from database
 list_bx = Listbox(root, height=16,width=40, font="helvetica 13", bg="light blue")
 list_bx.grid(row=3, column=1, columnspan=14, sticky=W+E, pady=40, padx=15)
 
+# Add scrollbar to enable scrolling
 scroll_bar = Scrollbar(root)
 scroll_bar.grid(row=1, column=8, rowspan=14,sticky=W)
 
-list_bx.configure(yscrollcommand=scroll_bar.set)
+list_bx.configure(yscrollcommand=scroll_bar.set)    # enable vertical scrolling
 scroll_bar.configure(command=list_bx.yview)
 
+# Add more Button widgets
 modify_btn = Button(root, text="Modify Record", bg="purple", fg="white", font="helvetica 10 bold", command="")
 modify_btn.grid(row=15, column=4)
 
